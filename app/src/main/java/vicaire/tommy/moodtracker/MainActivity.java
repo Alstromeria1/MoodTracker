a52aee1b2cdee4006a1c2b7e3e2ba06fb60336f5
Alstromeria <Tommy.vicaire@gmail.com>
2019-01-22 09:21:23 +0100

    The MainActivity now show a Smiley

--- app/src/main/java/vicaire/tommy/moodtracker/MainActivity.java
+++ app/src/main/java/vicaire/tommy/moodtracker/MainActivity.java
@@ -2,12 +2,27 @@ package vicaire.tommy.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout mMoodBackGround;
+    ImageView mMoodImage;
+    Button mCommentButton;
+    Button mHistoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoodBackGround = findViewById(R.id.mood_backGround);
+        mMoodImage = findViewById(R.id.mood_img);
+
+
+
+
     }
 }
