2a3a4010ad389bad320329869548eaf4ebd1197f
Alstromeria <Tommy.vicaire@gmail.com>
2019-01-22 10:52:36 +0100

    - Added swipe mechanics - Using the swipe change the color and the image of the main activity

--- app/src/main/java/vicaire/tommy/moodtracker/MainActivity.java
+++ app/src/main/java/vicaire/tommy/moodtracker/MainActivity.java
@@ -1,30 +1,129 @@
 package vicaire.tommy.moodtracker;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    RelativeLayout mMoodBackGround;
-    ImageView mMoodImage;
-    Button mCommentButton;
-    Button mHistoryButton;
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+
+   private RelativeLayout mMoodBackGround;
+   private ImageView mMoodImage;
+   private Button mCommentButton;
+   private Button mHistoryButton;
+   private int mCurrentIndex = 1;
+   private final int DELTA_MIN = 50;
+   private final int MOOD_LENGTH = 4;
+   GestureDetectorCompat mGestureDetectorCompat;
+
+
+   private final ArrayList<Mood> mMoodArrayList = new ArrayList<>();
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mGestureDetectorCompat = new GestureDetectorCompat(this , this);
         mMoodBackGround = findViewById(R.id.mood_backGround);
         mMoodImage = findViewById(R.id.mood_img);
 
-        mMoodBackGround.setBackgroundColor(getResources().getColor(R.color.light_sage));
+
+        mMoodArrayList.add(new Mood(getResources().getColor(R.color.banana_yellow), R.drawable.smiley_super_happy)) ;
+        mMoodArrayList.add(new Mood(getResources().getColor(R.color.light_sage), R.drawable.smiley_happy))      ;
+        mMoodArrayList.add(new Mood(getResources().getColor(R.color.cornflower_blue_65), R.drawable.smiley_normal)) ;
+        mMoodArrayList.add(new Mood(getResources().getColor(R.color.warm_grey), R.drawable.smiley_disappointed))    ;
+        mMoodArrayList.add(new Mood(getResources().getColor(R.color.faded_red), R.drawable.smiley_sad))  ;
+
+
+        mMoodBackGround.setBackgroundColor(mMoodArrayList.get(mCurrentIndex).getMoodBackGroundColor());
 
 
 
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return this.mGestureDetectorCompat.onTouchEvent(event) || super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float deltaX = e2.getX() - e1.getX();
+        float deltaY = e2.getY() - e1.getY();
+        if (Math.abs(deltaX) >= Math.abs(deltaY)) {
+            return false;
+        } else {
+
+            if (Math.abs(deltaY) >= DELTA_MIN) {
+                if (deltaY < 0) {
+                    if (mCurrentIndex < MOOD_LENGTH && mCurrentIndex >= 0) {
+                        mCurrentIndex += 1;
+                    }
+
+                    switchMood();
+                    return true;
+                } else {
+                    if (mCurrentIndex <= MOOD_LENGTH && mCurrentIndex > 0) {
+                        mCurrentIndex -= 1;
+                    }
+                    
+                    switchMood();
+                    return true;
+                }
+            }
+        }
+
+
+
+        return false;
+
+
+    }
+
+    private void switchMood(){
+        mMoodImage.setImageResource(mMoodArrayList.get(mCurrentIndex).getMoodImage());
+        mMoodBackGround.setBackgroundColor(mMoodArrayList.get(mCurrentIndex).getMoodBackGroundColor());
 
     }
 }
